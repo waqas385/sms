7a39df0519d4bb173a99d407e29a98f71ec0225e
waqas385 <waqas385@gmail.com>
2024-07-20 13:06:19 +0500

    First run server deploy

--- src/db.config.js
+++ src/db.config.js
@@ -1,9 +1,11 @@
 const dbConfig = {
   host: 'localhost',
-  user: 'root',
-  password: 'abc123',
-  database: 'inventory',
-  dateStrings: true // to return date as from db table otherwise https://github.com/sidorares/node-mysql2/issues/262#issuecomment-241604623
+  user: 'waqasah1_pmsuser',
+  password: '1a7x?keyrz4^',
+  database: 'waqasah1_inventory',
+//   debug: true,
+  dateStrings: true // to return date as from db table otherwise https://github.com/sidorares/node-mysql2/issues/262#issuecomment-241604$
 };
+
 
 export default dbConfig
